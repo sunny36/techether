08309ef724da6df984394f3db6f2828474359c36
AnthonyBobsin <bobsinj@gmail.com>
2015-02-13 16:22:19 -0500

    Fixing dropdown

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,7 +12,6 @@
 //
 //= require jquery
 //= require jquery-ujs
-//= require bootstrap-sass-official/assets/javascripts/bootstrap-sprockets
 //= require bootstrap
 //= require turbolinks
 //= require_tree .
