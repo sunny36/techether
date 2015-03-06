01678b515dae4880970e95def6108cdaceafb097
AnthonyBobsin <bobsinj@gmail.com>
2015-03-06 00:39:47 -0500

    Secret key token

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,4 +1,4 @@
-Rails.Application.config.secret_key_base = if Rails.env.development? or Rails.env.test? # generate simple key for test and development environments
+Rails.application.config.secret_key_base = if Rails.env.development? or Rails.env.test? # generate simple key for test and development environments
                                                     ('a' * 30) # should be at least 30 chars long
                                                   else
                                                     ENV['SECRET_TOKEN']
