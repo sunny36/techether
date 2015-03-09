5ccfa59fb3ddf1c1124b9dd651cbe96d1b73ecba
AnthonyBobsin <bobsinj@gmail.com>
2015-03-08 23:43:25 -0400

    Fixed mainfooter

--- app/assets/javascripts/static_pages.js
+++ app/assets/javascripts/static_pages.js
@@ -1,2 +1,15 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+$(document).ready(function () {
+    SetScrollableDivHeight($("#mainfooter"));
+});
+$(window).resize(function () {
+    SetScrollableDivHeight($("#mainfooter"));
+});
+function SetScrollableDivHeight(div) {
+    var top = div.position().top;
+    var windowH = $(document).height();
+    var remaining = windowH - top;
+
+    div.height(remaining - 25);
+}
