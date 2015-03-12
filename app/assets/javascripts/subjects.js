61d02a08472ee5d7846d3c0588ebbff7a3603be5
AnthonyBobsin <bobsinj@gmail.com>
2015-03-12 11:31:45 -0400

    Added some restrictions to new subject page

--- app/assets/javascripts/subjects.js
+++ app/assets/javascripts/subjects.js
@@ -1,2 +1,43 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+function CountChecks(which) {
+    var maxchecked = 2;
+    var count = 0;
+    var box;
+    if(document.getElementById("new_subject").gp.checked == true) {
+        if( which == "gp" ) { box = document.getElementById("new_subject").gp; }
+        count++;
+    }
+    if(document.getElementById("new_subject").wd.checked == true) {
+        if( which == "wd" ) { box = document.getElementById("new_subject").wd; }
+        count++;
+    }
+    if(document.getElementById("new_subject").md.checked == true) {
+        if( which == "md" ) { box = document.getElementById("new_subject").md; }
+        count++;
+    }
+    if(document.getElementById("new_subject").f.checked == true) {
+        if( which == "f" ) { box = document.getElementById("new_subject").f; }
+        count++;
+    }
+    if(document.getElementById("new_subject").mo.checked == true) {
+        if( which == "mo" ) { box = document.getElementById("new_subject").mo; }
+        count++;
+    }
+    if(document.getElementById("new_subject").dm.checked == true) {
+        if( which == "dm" ) { box = document.getElementById("new_subject").dm; }
+        count++;
+    }
+    if(document.getElementById("new_subject").cms.checked == true) {
+        if( which == "cms" ) { box = document.getElementById("new_subject").cms; }
+        count++;
+    }
+    if(document.getElementById("new_subject").other.checked == true) {
+        if( which == "other" ) { box = document.getElementById("new_subject").other; }
+        count++;
+    }
+    if(count > maxchecked) {
+        box.checked = false;
+        alert('Sorry, only ' + maxchecked + ' may be checked.');
+    }
+}
