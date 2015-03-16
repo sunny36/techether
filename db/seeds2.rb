5c6a53d8187bc478a6b08f982c354753e331d5af
AnthonyBobsin <bobsinj@gmail.com>
2015-03-15 21:15:19 -0400

    Removed gitignore

--- db/seeds2.rb
+++
@@ -1,7 +0,0 @@
-User.where("email LIKE ?", "%example%").to_a.each do | user |
-  Subject.find(user.subject_id).resources.to_a.each do | resource |
-    Rating.create!(value: rand(4..5),
-                   resource_id: resource.id,
-                   user_id: user.id)
-  end
-end
