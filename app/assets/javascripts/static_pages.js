23962db8e081aeed241efef9d290ac5638a1a324
AnthonyBobsin <bobsinj@gmail.com>
2015-04-15 00:49:19 -0400

    Better UI

--- app/assets/javascripts/static_pages.js
+++ app/assets/javascripts/static_pages.js
@@ -1,3 +1,15 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 
+$(document).ready(function() {
+    $('.rightnav').hover(function() {
+        $(this).siblings('.bottomBorder').addClass('bottomBorderAnimation');
+    }, function() {
+        $(this).siblings('.bottomBorder').removeClass('bottomBorderAnimation');
+    });
+    $('.rightnav').focus(function() {
+        $(this).siblings('.bottomBorder').addClass('bottomBorderAnimation');
+    }, function() {
+        $(this).siblings('.bottomBorder').removeClass('bottomBorderAnimation');
+    });
+});
