612a79241faa861169a723969c6c48e9bfa09e61
Anthony Bobsin <bobsinj@gmail.com>
2015-10-26 23:07:40 +0000

    Begins user controller spec by testing that the correct user gets retrieved

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,12 +1,21 @@
 require 'rails_helper'
 
 describe UsersController, :type => :controller do
+  let(:user) { Fabricate(:user) }
+
   describe "GET show/:id" do
     it "responds successfully with an HTTP 200 status code" do
-      user = Fabricate(:user)
       get :show, id: user.id
-      require 'byebug'; byebug
       expect(response).to have_http_status(200)
+    end
+
+    it "retrieves the correct user" do
+      get :show, id: user.id
+      expect(response).to have_http_status(200)
+
+      response_user = subject.instance_variable_get(:@user)
+      expect(response_user).to_not be_nil
+      expect(response_user).to eq(user)
     end
   end
 end
